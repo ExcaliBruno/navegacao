860d52f51c4a60fde7e2ed56786e28ecc83b96c4
Bruno <brunohep86@gmail.com>
2025-05-08 21:51:05 +0200

    Feat: navegacao usando Drawer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
-//import Stack from './Stack'
+
+//import Drawer from './Drawer'
 import Tab from './Tab'
+//import Stack from './Stack'
 
 export default props => (
     <SafeAreaView style = {{flex:1}}>
         <NavigationContainer>
             <Tab />
-            {/* <Stack /> */}
+            {/*
+                <Drawer />
+               <Stack /> */}
         </NavigationContainer>
     </SafeAreaView>
 )
