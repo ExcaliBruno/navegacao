6e7522d4b62f62191e74e7437fdd6cbc6ccc192b
Bruno <brunohep86@gmail.com>
2025-05-08 20:37:19 +0200

    feat: navegacao utilizando BottomTab

---
+++ src/routes/Tab.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TelaA from '../views/TelaA';
+import TelaB from '../views/TelaB';
+import TelaC from '../views/TelaC';
+
+const Tab = createBottomTabNavigator();
+
+export default () => (
+  <Tab.Navigator
+    screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor: 'red',
+      tabBarInactiveTintColor: 'blue',
+      tabBarLabelStyle: {fontSize: 30},
+      tabBarStyle: {height: 70},
+      //tabBarIcon: () => null, // Remove ícone e o espaço dele
+    }} initialRouteName="TelaB"
+  >
+    <Tab.Screen name="TelaA" component={TelaA} options={{ tabBarLabel: 'Tela A' }} />
+    <Tab.Screen name="TelaB" component={TelaB} options={{ tabBarLabel: 'Tela B' }} />
+    <Tab.Screen name="TelaC" component={TelaC} options={{ tabBarLabel: 'Tela C' }} />
+  </Tab.Navigator>
+);
