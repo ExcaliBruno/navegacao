8181320049935f9fa9c37e1a4f187a389ed3fe72
Bruno <brunohep86@gmail.com>
2025-05-08 14:14:15 +0200

    feat: Navegacao de 3 telas

--- src/routes/Stack.js
+++ src/routes/Stack.js
@@ -3,16 +3,30 @@ import { createStackNavigator }from '@react-navigation/stack'
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
+import PassoStack from '../components/PassoStack'
 
 const Stack = createStackNavigator()
 
 export default props => (
     <Stack.Navigator initialRouteName="TelaA"
-        screenOptions={{ headerShown: false}}>
+        screenOptions={{ headerShown: true}}
+        >
         <Stack.Screen name="TelaA" 
-            options={{ title: 'Informacoes Iniciais' }}
-            component={TelaA} />
-        <Stack.Screen name="TelaB" component={TelaB} />
+            options={{ title: 'Informacoes Iniciais' }}>
+            {props => (
+                <PassoStack {...props} avancar="TelaB">
+                    <TelaA />
+                </PassoStack>
+            )}
+        </Stack.Screen>
+        <Stack.Screen name="TelaB">
+            {props => (
+                <PassoStack {...props} avancar="TelaC">
+                    <TelaB />
+                </PassoStack>
+            )}
+
+        </Stack.Screen>
         <Stack.Screen name="TelaC" component={TelaC} />
     </Stack.Navigator>
 )
