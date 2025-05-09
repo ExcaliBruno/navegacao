b8bb7cd9508d897fe80a4c9a213392259861abfa
Bruno <brunohep86@gmail.com>
2025-05-09 14:12:24 +0200

    Feat: adicionado icones na tabBar

--- src/routes/Tab.js
+++ src/routes/Tab.js
@@ -3,19 +3,31 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
 import TelaC from '../views/TelaC';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default () => (
   <Tab.Navigator
-    screenOptions={{
+    screenOptions={({ route }) => ({
       headerShown: false,
       tabBarActiveTintColor: 'red',
       tabBarInactiveTintColor: 'blue',
-      tabBarLabelStyle: {fontSize: 30},
-      tabBarStyle: {height: 70},
-      //tabBarIcon: () => null, // Remove ícone e o espaço dele
-    }} initialRouteName="TelaB"
+      tabBarLabelStyle: { fontSize: 20 },
+      tabBarStyle: { height: 70 },
+      tabBarIcon: ({ color, size }) => {
+        let iconName;
+        if (route.name === 'TelaA') {
+          iconName = 'home';
+        } else if (route.name === 'TelaB') {
+          iconName = 'list';
+        } else if (route.name === 'TelaC') {
+          iconName = 'settings';
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+    initialRouteName="TelaB"
   >
     <Tab.Screen name="TelaA" component={TelaA} options={{ tabBarLabel: 'Tela A' }} />
     <Tab.Screen name="TelaB" component={TelaB} options={{ tabBarLabel: 'Tela B' }} />
